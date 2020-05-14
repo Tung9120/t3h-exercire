9a023d961d14f7c41bbf16e742fa444d6f7034a4
tung9120 <vutung959595@gmail.com>
2020-05-15 04:58:49 +0700

    code on class - js day 1

---
+++ code-on-class/js-day-1/index.js
@@ -0,0 +1,36 @@
+// alert('Hello Class');
+
+var helloMessage = 'Hello Class';
+
+console.log(helloMessage);
+var space = (100 + 20 - (40 - 10)) * 4 / 2;
+var velociy = -5;
+var timer = space / velociy;
+
+console.log('timer = ' + timer);
+console.log('remainer = ', 5 % 3);
+
+var index = 0;
+index++;
+++index;
+console.log('index ', index);
+
+var a = 0;
+var a1 = a++;
+var a2 = ++a;
+
+console.log('a = ', a, ', a1 = ', a1, ', a2 = ', a2);
+
+var b = 5; // a = 2; b = 5
+var c = b++ + ++b + --a + a-- + a;
+console.log('c = ',c);
+
+var name = 'Tung';
+var Name = 'Tung';
+
+let myNumber = 5;
+let mynumber = 5;
+
+const PI = 3.14;
+
+hihi = 'HiHi';

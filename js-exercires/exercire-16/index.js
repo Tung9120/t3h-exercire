1fc322d42a524f88cbf0f9f9e838542ede9d305a
tung9120 <vutung959595@gmail.com>
2020-05-31 19:08:26 +0700

    Updated code for js-exercire-16

--- js-exercires/exercire-16/index.js
+++ js-exercires/exercire-16/index.js
@@ -3,7 +3,7 @@ const url = 'https://www.facebook.com/ngothucdat';
 const getId = (url) => {
     const domain = 'https://www.facebook.com/';
     let id = url.slice(domain.length, url.length);
-    console.log(id);
+    return id;
 }
 
-getId(url);
+console.log('getId("https://www.facebook.com/ngothucdat"):', getId("https://www.facebook.com/ngothucdat"));

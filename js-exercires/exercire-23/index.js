eea9269914628f83c218f8565cd65a903bc16399
tung9120 <vutung959595@gmail.com>
2020-06-01 09:05:56 +0700

    Completed js-exercire-23

--- js-exercires/exercire-23/index.js
+++ js-exercires/exercire-23/index.js
@@ -1,5 +1,20 @@
-const a = [1,2,3,4,5,6,7,8,9,9,9,8,7,6,5,4,3,2,1];
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
-const mostOccur = arr => {
+const mostOccur = (arr) => {
+  let occurences = [];
+  for(let i = 0; i < arr.length; i++){
+    occurences[arr[i]] = (occurences[arr[i]] || 0) + 1;
+  }
+  occurences.shift();
+  let maxValue = occurences[0];
   
+  for (let i = 0; i < occurences.length; i++) {
+    if (occurences[i] > maxValue) {
+      maxValue = occurences[i];
+    }
+  }
+
+  return maxValue;
 };
+
+console.log('mostOccur(a):', mostOccur(a));

0cee2241567dd510f9a11c8c8567a7ffa7678a9d
tung9120 <vutung959595@gmail.com>
2020-06-02 13:54:52 +0700

    Completed js-exercire-28

---
+++ js-exercires/exercire-28/index.js
@@ -0,0 +1,26 @@
+const students = [
+  {
+    id: "T3HXX1",
+    firstName: "NgAN",
+    lastName: "Duong Thuy",
+  },
+  {
+    id: "T3HXX2",
+    firstName: "Ha",
+    lastName: "Do Thi Thu",
+  },
+  {
+    id: "T3HXX5",
+    firstName: "Minh",
+    lastName: "Nguyen Nhat",
+  },
+];
+
+const filterSpecialStudents = (name) => {
+  const { lastName } = name;
+  return lastName.indexOf('Do') !== -1;
+};
+
+const specialStudents = students.filter(filterSpecialStudents);
+
+console.log("specialStudents", specialStudents);
